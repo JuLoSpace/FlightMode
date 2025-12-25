a4468762694b4f3b353de507217b8a962ba173f2
Yaroslav Soloviev <julospace@gmail.com>
2025-12-25 21:47:34 +0300

    Initial Commit

---
+++ FlightMode/FlightModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightModeApp.swift
+//  FlightMode
+//
+//  Created by Ярослав Соловьев on 25.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct FlightModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
